1630936109f65855a89a053c911e2c6d2d742683
mamouthan <78535180+mamouthan@users.noreply.github.com>
2021-06-22 19:23:52 +0800

    Update Program.cs
    - Remove if else for selected calculation type
    + Add Dictionary for calculation types
    + Update main code to get method from userInput.cs

--- IntroUI/Program.cs
+++ IntroUI/Program.cs
@@ -6,30 +6,18 @@ namespace IntroUI
     class Program
     {
         static void Main(string[] args)
-        {   
-            Console.WriteLine("Please input numbers then enter \" = \" when done: ");
-            Tuple<double[],string> userInput = GetKeyboardInput();
-            double[] numbers = userInput.Item1;
-            string choosedOutput = userInput.Item2;
-            double outputMethod = 0;
-            if (choosedOutput == "1")
+        {
+            userInput userInput = new userInput();
+            double[] numbers = userInput.GetKeyboardInput();
+            string choosedOutput = userInput.GetSelectionCalculation();
+            Dictionary<string, Func<double[], double>> calculationDictionary = new Dictionary<string, Func<double[], double>>
             {
-                choosedOutput = "highest number";
-                outputMethod = FindHighestNumber(numbers);
-            }
-            else if (choosedOutput == "2")
-            {
-                choosedOutput = "lowest number";
-                outputMethod = FindLowestNumber(numbers);
-            }
-            else if (choosedOutput == "3")
-            {
-                choosedOutput = "average number";
-                outputMethod = FindAverageNumber(numbers);
-            }
-
-            Console.WriteLine("===========================");
-            Console.WriteLine("The " + choosedOutput + " is " + outputMethod);
+                {"1", FindHighestNumber},
+                {"2", FindLowestNumber},
+                {"3", FindAverageNumber}
+            };
+            double resultCalculation = calculationDictionary[choosedOutput](numbers);
+            Console.WriteLine("The result is {0}", resultCalculation);
         }
         static double FindHighestNumber(double[] listInput)
         {
@@ -63,51 +51,8 @@ namespace IntroUI
             {
                 totalNumber += item;
             }
-            double averageNumber = totalNumber/totalCount;
+            double averageNumber = totalNumber / totalCount;
             return averageNumber;
-        }
-        static Tuple<double[], string> GetKeyboardInput()
-        {
-            List<double> listNumbers = new List<double>();
-            while (true)
-            {
-                string keyboardInput = Console.ReadLine();
-                Boolean validNumber = double.TryParse(keyboardInput, out double compareNumber);
-                if(!validNumber && keyboardInput != "=")
-                {
-                    Console.WriteLine("Not a Number, please try again.");
-                    continue;
-                }
-                if (keyboardInput == "=")
-                {
-                    break;
-                }
-                else
-                {
-                    listNumbers.Add(Convert.ToDouble(keyboardInput));
-                }
-            }
-            double[] numbers = listNumbers.ToArray();
-
-            Console.WriteLine("Please choose output for numbers input:");
-            Console.WriteLine("1. Highest Number");
-            Console.WriteLine("2. Lowest Number");
-            Console.WriteLine("3. Average Number");
-            string choosedMethod = "";
-            while (true)
-            {
-                string outputType = Console.ReadLine();
-                if (outputType == "1" || outputType == "2" || outputType == "3")
-                {
-                    choosedMethod = outputType;
-                    break;
-                }
-                else
-                {
-                    Console.WriteLine(" You choosed wrong method, please choose again 1, 2 or 3");
-                }
-            }
-            return Tuple.Create(numbers, choosedMethod);
         }
     }
 }
