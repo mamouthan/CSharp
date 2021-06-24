cec468620ff3687ff4e0a0b6b1d76b83b2078102
mamouthan <78535180+mamouthan@users.noreply.github.com>
2021-06-24 16:43:02 +0800

    Add files via upload
    +Add UserInputHelper.cs

---
+++ IntroUI/UserInputHelper.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace numbers
+{
+    public class UserInputHelper
+    {
+        UserInput userInput = new UserInput();
+        public void GetInputKeyboard()
+        {
+            Console.WriteLine("Please input numbers then enter \" = \" when done: ");
+            List<double> listNumbers = new List<double>();
+            while (true)
+            {
+                string keyboardInput = Console.ReadLine();
+                bool validNumber = double.TryParse(keyboardInput, out double compareNumber);
+                if (!validNumber && keyboardInput != "=")
+                {
+                    Console.WriteLine("Not a Number, please try again.");
+                    continue;
+                }
+                if (keyboardInput == "=")
+                {
+                    break;
+                }
+                else
+                {
+                    listNumbers.Add(Convert.ToDouble(keyboardInput));
+                }
+            }
+            userInput.Numbers = listNumbers.ToArray();
+        }
+        public void GetSelectionCalculation()
+        {
+            Console.WriteLine("Please choose output for numbers input:");
+            Console.WriteLine("1. Highest Number");
+            Console.WriteLine("2. Lowest Number");
+            Console.WriteLine("3. Average Number");
+            while (true)
+            {
+                string keyboardInput = Console.ReadLine();
+                if (keyboardInput == "1" || keyboardInput == "2" || keyboardInput == "3")
+                {
+                    userInput.ChoosedCalculation = keyboardInput;
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine(" You choosed wrong method, please choose again 1, 2 or 3");
+                }
+            }
+        }
+    }
+}
